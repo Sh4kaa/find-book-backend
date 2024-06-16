ad278b5086700a4bc994cfc05d7f4ddadaed117f
Dario de souza <darioo_dario@hotmail.com>
2024-06-16 10:53:08 -0300

    chore: rotas de livros

---
+++ src/infra/router/books.routes.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { BooksController } from "../../app/controllers/books.controller";
+import { routerAdapter } from "./routerAdapter";
+
+export const BookRoutes = (router: Router) => {
+  const booksController = new BooksController();
+  router.post("/books", routerAdapter(booksController, "create"));
+};
